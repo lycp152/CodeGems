6dc821782602d868cf36472673766a629ebe14b0
lycp152 <118104209+lycp152@users.noreply.github.com>
2023-08-23 20:18:34 +0900

    タイトル画面の追加

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,11 @@
 import "./styles/Home.css";
+import Title from "./pages/Title"; // Titleコンポーネントをインポート
 
 export default function Home() {
   return (
     <main className="main">
       <div className="main-contents">
-        <h1 className="title">CodeGems</h1>
+        <Title />
       </div>
       <footer>
         <p>Copyright © 2023 🧙草咲か爺さんズ All Rights Reserved.</p>
