97fced97ff0707f2ae3359e82ce9f05d0d521f6a
lycp152 <118104209+lycp152@users.noreply.github.com>
2023-08-23 23:03:04 +0900

    タイトル画面のレイアウト更新

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,14 @@
+import React from "react";
 import "./styles/Home.css";
-import Title from "./pages/Title"; // Titleコンポーネントをインポート
+import Title from "./pages/Title";
+import SideMenuButton from "./components/SideMenuButton";
 
 export default function Home() {
   return (
     <main className="main">
       <div className="main-contents">
         <Title />
+        <SideMenuButton />
       </div>
       <footer>
         <p>Copyright © 2023 🧙草咲か爺さんズ All Rights Reserved.</p>
---
+++ front/src/components/SideMenuButton.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function SideMenuButton() {
+  return (
+    <div className="side-buttons">
+      {/* ここにボタンを追加 */}
+      <button className="side-button">Button 1</button>
+      <button className="side-button">Button 2</button>
+      {/* 必要に応じて他のボタンを追加 */}
+    </div>
+  );
+}
