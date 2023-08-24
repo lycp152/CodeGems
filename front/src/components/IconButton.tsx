93ce319fdeb81ace2e82aa4516b79629d3bcffb5
lycp152 <118104209+lycp152@users.noreply.github.com>
2023-08-24 23:32:13 +0900

    ボタンのレイアウトを更新

--- front/src/components/IconButton.tsx
+++ front/src/components/IconButton.tsx
@@ -8,7 +8,7 @@ interface IconButtonProps {
 
 const IconButton: React.FC<IconButtonProps> = ({ icon, label, onClick }) => {
   return (
-    <button className="side-button" onClick={onClick}>
+    <button className="icon-button" onClick={onClick}>
       <div className="button-content">
         <div className="icon">{icon}</div>
         <div className="text">{label}</div>
--- front/src/pages/Title.tsx
+++ front/src/pages/Title.tsx
@@ -4,6 +4,10 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import { useAuth } from "../context/AuthContext";
 import { usePlay } from "../context/PlayContext";
 import LongButton from "../components/LongButton";
+import IconButton from "../components/IconButton";
+import MilitaryTechIcon from "@mui/icons-material/MilitaryTech";
+import DiamondIcon from "@mui/icons-material/Diamond";
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
 
 export default function Title() {
   const { isLoggedIn, setIsLoggedIn } = useAuth();
@@ -26,7 +30,23 @@ export default function Title() {
             onClick={handleLogin}
           />
         ) : (
-          <LongButton label="Play" onClick={handlePlay} />
+          <>
+            <div className="main-menu">
+              <IconButton
+                icon={<MilitaryTechIcon style={{ fontSize: 80 }} />}
+                label="rewardNFT"
+              />
+              <IconButton
+                icon={<DiamondIcon style={{ fontSize: 80 }} />}
+                label="gemSkin"
+              />
+              <IconButton
+                icon={<EmojiEventsIcon style={{ fontSize: 80 }} />}
+                label="ranking"
+              />
+            </div>
+            <LongButton label="Play" onClick={handlePlay} />
+          </>
         )}
       </div>
     </div>
