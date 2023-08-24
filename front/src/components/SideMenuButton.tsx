5caddd20e8e2c18e7c94f07fe6ddbb1db5dbf381
lycp152 <118104209+lycp152@users.noreply.github.com>
2023-08-24 21:04:11 +0900

    ボタンをコンポーネント化

--- front/src/components/SideMenuButton.tsx
+++ front/src/components/SideMenuButton.tsx
@@ -1,10 +1,12 @@
+import React from "react";
+import IconButton from "./IconButton";
 import LightbulbIcon from "@mui/icons-material/Lightbulb";
 import LogoutIcon from "@mui/icons-material/Logout";
 import VolumeUpIcon from "@mui/icons-material/VolumeUp";
 import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
-import "../styles/SideMenuButton.css";
 import { useAuth } from "../context/AuthContext";
 import { usePlay } from "../context/PlayContext";
+import "../styles/SideMenuButton.css";
 
 export default function SideMenuButton() {
   const { isLoggedIn, setIsLoggedIn } = useAuth();
@@ -12,41 +14,29 @@ export default function SideMenuButton() {
     setIsLoggedIn(false);
   };
   const { isPlaying } = usePlay();
+
   return (
     <div className="side-buttons">
       {isPlaying ? (
-        <button className="side-button">
-          <div className="button-content">
-            <div className="icon">
-              <LightbulbIcon style={{ fontSize: 80 }} />
-            </div>
-            <div className="text">hint</div>
-          </div>
-        </button>
+        <IconButton
+          icon={<LightbulbIcon style={{ fontSize: 80 }} />}
+          label="hint"
+        />
       ) : isLoggedIn ? (
-        <button onClick={handleLogout} className="side-button">
-          <div className="button-content">
-            <LogoutIcon style={{ fontSize: 80 }} />
-            logOut
-          </div>
-        </button>
+        <IconButton
+          onClick={handleLogout}
+          icon={<LogoutIcon style={{ fontSize: 80 }} />}
+          label="logOut"
+        />
       ) : null}
-      <button className="side-button">
-        <div className="button-content">
-          <div className="icon">
-            <VolumeUpIcon style={{ fontSize: 80 }} />
-          </div>
-          <div className="text">sound</div>
-        </div>
-      </button>
-      <button className="side-button">
-        <div className="button-content">
-          <div className="icon">
-            <QuestionMarkIcon style={{ fontSize: 80 }} />
-          </div>
-          <div className="text">howToPlay</div>
-        </div>
-      </button>
+      <IconButton
+        icon={<VolumeUpIcon style={{ fontSize: 80 }} />}
+        label="sound"
+      />
+      <IconButton
+        icon={<QuestionMarkIcon style={{ fontSize: 80 }} />}
+        label="howToPlay"
+      />
     </div>
   );
 }
