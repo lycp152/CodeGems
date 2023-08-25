86d5f1d4605bf4bcb619fb27dfe8b3da28cc5821
lycp152 <118104209+lycp152@users.noreply.github.com>
2023-08-25 09:27:22 +0900

    音声のオンオフの制御を追加

--- front/src/components/SideMenuButton.tsx
+++ front/src/components/SideMenuButton.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import IconButton from "./IconButton";
 import LightbulbIcon from "@mui/icons-material/Lightbulb";
 import LogoutIcon from "@mui/icons-material/Logout";
 import VolumeUpIcon from "@mui/icons-material/VolumeUp";
+import VolumeOffIcon from "@mui/icons-material/VolumeOff";
 import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack"; // 追加：タイトルへ戻るボタンのアイコン
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { useAuth } from "../context/AuthContext";
 import { usePlay } from "../context/PlayContext";
 import "../styles/SideMenuButton.css";
@@ -16,7 +17,13 @@ export default function SideMenuButton() {
   };
   const { isPlaying } = usePlay();
 
-  const isDetailView = false; // メニューの詳細画面かどうか判定
+  const isDetailView = false;
+
+  const [isSoundOn, setIsSoundOn] = useState(true);
+
+  const handleSoundToggle = () => {
+    setIsSoundOn((prevState) => !prevState);
+  };
 
   return (
     <div className="side-buttons">
@@ -35,7 +42,14 @@ export default function SideMenuButton() {
             />
           ) : null}
           <IconButton
-            icon={<VolumeUpIcon style={{ fontSize: 80 }} />}
+            onClick={handleSoundToggle}
+            icon={
+              isSoundOn ? (
+                <VolumeUpIcon style={{ fontSize: 80 }} />
+              ) : (
+                <VolumeOffIcon style={{ fontSize: 80 }} />
+              )
+            } // 音声アイコンの切り替え
             label="sound"
           />
           <IconButton
