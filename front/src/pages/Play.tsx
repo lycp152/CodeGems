d83240acfaa3c9da4f80555ea4ac92af1c8953e6
lycp152 <118104209+lycp152@users.noreply.github.com>
2023-09-01 09:03:36 +0900

    コメントを追加

--- front/src/pages/Play.tsx
+++ front/src/pages/Play.tsx
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from "react";
 import "../styles/Play.css";
 
-const numRows = 8; // グリッドの行数
-const numCols = 7; // グリッドの列数
-const numGemTypes = 6; // ジェムの種類数
+// グリッドの行数、列数、ジェムの種類数を定義
+const numRows = 8;
+const numCols = 7;
+const numGemTypes = 6;
 
+// ジェムの型を定義
 interface Gem {
   gemValue: number;
   backgroundColor: string;
 }
 
+// ジェムの背景色とジェムの値のマッピング
 const gemBackgroundColors: Record<string, number> = {
   "#12151A": 1,
   "#0E351F": 2,
@@ -38,25 +41,28 @@ interface PlayProps {
   score: number;
   setScore: React.Dispatch<React.SetStateAction<number>>;
 }
-
+// ゲームコンポーネント
 const Play: React.FC<PlayProps> = ({
   remainingTime,
   setRemainingTime,
   score,
   setScore,
 }) => {
-  const [grid, setGrid] = useState<Gem[][]>([]); // グリッドの状態
+  // グリッドと選択されたジェムの状態を管理するState
+  const [grid, setGrid] = useState<Gem[][]>([]);
   const [selectedGem, setSelectedGem] = useState<{
     row: number;
     col: number;
-  } | null>(null); // 選択されたジェムの位置
+  } | null>(null);
 
+  // コンポーネントの初回レンダリング時およびremainingTimeが変更されるたびに実行されるEffect
   useEffect(() => {
     // グリッドを初期化
     const newGrid: Gem[][] = [];
     for (let row = 0; row < numRows; row++) {
       const newRow: Gem[] = [];
       for (let col = 0; col < numCols; col++) {
+        // 隣接するジェムの値を除外してランダムなジェムを生成
         const excludedValues: number[] = [];
         if (row >= 2) {
           const previousGems = [newGrid[row - 1][col], newGrid[row - 2][col]];
@@ -73,9 +79,10 @@ const Play: React.FC<PlayProps> = ({
       }
       newGrid.push(newRow);
     }
+    // グリッドを更新
     setGrid(newGrid);
 
-    // タイマーを設定
+    // 1秒ごとにremainingTimeを減少させるタイマーを設定
     const timer = setInterval(() => {
       setRemainingTime((prevTime) => (prevTime > 0 ? prevTime - 1 : prevTime));
     }, 1000);
@@ -84,8 +91,9 @@ const Play: React.FC<PlayProps> = ({
     return () => {
       clearInterval(timer);
     };
-  }, [setRemainingTime]);
+  }, [setRemainingTime]); // setRemainingTimeが変更されるとEffectが実行される
 
+  // 残り時間を分:秒のフォーマットに変換する関数
   function formatTime(seconds: number): string {
     const minutes = Math.floor(seconds / 60);
     const remainingSeconds = seconds % 60;
@@ -94,6 +102,7 @@ const Play: React.FC<PlayProps> = ({
       .padStart(2, "0")}`;
   }
 
+  // ジェムがクリックされたときの処理
   function handleGemClick(row: number, col: number): void {
     if (selectedGem === null) {
       setSelectedGem({ row, col });
@@ -104,24 +113,26 @@ const Play: React.FC<PlayProps> = ({
         (Math.abs(selectedGem.col - col) === 1 && selectedGem.row === row);
 
       if (isAdjacent) {
-        // ジェムを交換
+        // ジェムを交換してグリッドを更新
         const updatedGrid = grid.map((gridRow, rowIndex) =>
           gridRow.map((gem, colIndex) => {
             if (
               (rowIndex === selectedGem.row && colIndex === selectedGem.col) ||
               (rowIndex === row && colIndex === col)
             ) {
-              return {
-                ...grid[selectedGem.row][selectedGem.col],
-                ...grid[row][col],
-              };
+              // 選択されたジェムとクリックされたジェムのプロパティをマージ
+              return Object.assign(
+                {},
+                grid[selectedGem.row][selectedGem.col],
+                grid[row][col]
+              );
             }
             return gem;
           })
         );
 
-        setGrid(updatedGrid);
-        setSelectedGem(null);
+        setGrid(updatedGrid); // グリッドを更新
+        setSelectedGem(null); // 選択されたジェムをリセット
       } else {
         setSelectedGem({ row, col });
       }
