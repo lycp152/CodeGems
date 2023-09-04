9349dd04b731df62b383cb5e9b2e09b70f794146
lycp152 <118104209+lycp152@users.noreply.github.com>
2023-09-04 18:23:17 +0900

    ランキング表示をコンポーネント化

---
+++ front/src/components/RankingContainer.tsx
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from "react";
+
+function RankingContainer() {
+  const [topScores, setTopScores] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchTopScores = () => {
+    // 仮のランキングデータを取得するロジックをここに配置
+    const mockData = [
+      { username: "User1", score: 1000 },
+      { username: "User2", score: 900 },
+      { username: "User1", score: 1000 },
+      { username: "User2", score: 900 },
+      { username: "User1", score: 1000 },
+      { username: "User2", score: 900 },
+      { username: "User1", score: 1000 },
+      { username: "User2", score: 900 },
+      { username: "User1", score: 1000 },
+      { username: "User2", score: 900 },
+      { username: "User1", score: 1000 },
+      { username: "User2", score: 900 },
+      { username: "User1", score: 1000 },
+      { username: "User2", score: 900 },
+      { username: "User1", score: 1000 },
+      { username: "User2", score: 900 },
+      { username: "User1", score: 1000 },
+      { username: "User2", score: 900 },
+      { username: "User1", score: 1000 },
+      { username: "User2", score: 900 },
+      { username: "User1", score: 1000 },
+      { username: "User2", score: 900 },
+      // 他のユーザーのデータを追加
+    ];
+    setTopScores(mockData);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchTopScores();
+  }, []);
+
+  const top10 = topScores.slice(0, 10);
+  const restOfTheRankings = topScores.slice(10, 20);
+
+  return (
+    <div className="ranking-container">
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <h2 className="ranking-subject">Top 20 Rankings</h2>
+          <div className="rankings">
+            <div className="top10">
+              <ol>
+                {top10.map((entry, index) => (
+                  <li key={index}>
+                    {entry.username}: {entry.score}
+                  </li>
+                ))}
+              </ol>
+            </div>
+            {restOfTheRankings.length > 0 && (
+              <div className="top11-20">
+                <ol start={11}>
+                  {restOfTheRankings.map((entry, index) => (
+                    <li key={index}>
+                      {entry.username}: {entry.score}
+                    </li>
+                  ))}
+                </ol>
+              </div>
+            )}
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default RankingContainer;
