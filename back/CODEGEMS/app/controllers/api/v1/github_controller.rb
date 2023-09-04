3e73e8f50b12f7d91c32fae6979f9b6d49491ce3
raiden <112748994+raiden314@users.noreply.github.com>
2023-09-05 08:01:16 +0900

    githubの認証機能追加

--- back/CODEGEMS/app/controllers/api/v1/github_controller.rb
+++ back/CODEGEMS/app/controllers/api/v1/github_controller.rb
@@ -3,16 +3,19 @@ module Api
         class GithubController < ApplicationController
             # HTTPClientを呼び出す
             require 'httpclient'
-            require 'json'
-            require 'httparty'
             def index
-                curl -sS https://github-contributions-api.deno.dev/[ユーザ名].json?flat=true | jq ".contributions[-3:]"
+                # curl -sS https://github-contributions-api.deno.dev/raiden314.json?flat=true | jq ".contributions[-3:]"
                 binding.pry
-                                
-                headers = {
-                    'Authorization' => "token #{access_token}",
-                    'User-Agent' => 'HTTParty'
-                }
+                      
+                client    = HTTPClient.new
+                url       = "https://github-contributions-api.deno.dev/raiden314.json?flat=true"
+                binding.pry
+                response  = client.get(url)
+                res_json  = JSON.parse(response.body)
+                # headers = {
+                #     'Authorization' => "token #{access_token}",
+                #     'User-Agent' => 'HTTParty'
+                # }
 
                 # query = '{query($userName:String!) {
                 #     user(login: $userName){
@@ -29,12 +32,12 @@ module Api
                 #       }
                 #     }
                 #   }}'
-                response = HTTParty.get(url,header: headers)
-                binding.pry
+                # response = HTTParty.get(url,header: headers)
+                # binding.pry
                 # レスポンスから草の数を取得
-                total_contributions = response.parsed_response.map { |c| c['total'] }.sum
+                # total_contributions = response.parsed_response.map { |c| c['total'] }.sum
                 # 草の数を表示
-                puts "Total contributions: #{total_contributions}"                # POSTするデータ
+                # puts "Total contributions: #{total_contributions}"                # POSTするデータ
                 
                 # JSON形式に変換
                 # json_data = JSON.generate(data)
--- back/CODEGEMS/config/routes.rb
+++ back/CODEGEMS/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
   end
+  post '/login/oauth/github', to: 'github_oauth#login'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
