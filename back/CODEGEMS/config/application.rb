001b38e8461f492599b5af341cd9431811dedeb7
raiden <112748994+raiden314@users.noreply.github.com>
2023-09-04 19:15:47 +0900

    graphqlのセットアップとgithubの草の取得方法を模索

--- back/CODEGEMS/config/application.rb
+++ back/CODEGEMS/config/application.rb
@@ -2,6 +2,8 @@ require_relative "boot"
 
 require "rails/all"
 
+require "sprockets/railtie"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -23,5 +25,7 @@ module CODEGEMS
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
--- back/CODEGEMS/config/routes.rb
+++ back/CODEGEMS/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  post "/graphql", to: "graphql#execute"
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -6,6 +10,7 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       resources :ranks
+      get '/github' =>  'github#index'
     end
   end
 end
