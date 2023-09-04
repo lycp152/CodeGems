0c005e87f2bb1cd6e466337ab13d7a19c3c2d8c4
raiden <112748994+raiden314@users.noreply.github.com>
2023-09-05 08:07:17 +0900

    skincategorysコントローラを作詞

---
+++ back/CODEGEMS/app/controllers/skincategorys_controller.rb
@@ -0,0 +1,2 @@
+class SkincategorysController < ApplicationController
+end
---
+++ back/CODEGEMS/test/controllers/skincategorys_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SkincategorysControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
