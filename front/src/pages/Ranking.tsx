82a0d2eaf466dd5f176d741e1c8ca0f837ddee5b
lycp152 <118104209+lycp152@users.noreply.github.com>
2023-09-20 14:34:35 +0900

    ランキングのエラー修正

--- front/src/pages/Ranking.tsx
+++ front/src/pages/Ranking.tsx
@@ -3,8 +3,6 @@ import RankingContainer from "../components/RankingContainer";
 import "../styles/Ranking.css";
 
 export default function Ranking() {
-  
-
   return (
     <div className="title-container">
       <h1 className="title">ranking</h1>
@@ -12,5 +10,6 @@ export default function Ranking() {
       <div className="main-container">
         <RankingContainer />
       </div>
+    </div>
   );
 }
